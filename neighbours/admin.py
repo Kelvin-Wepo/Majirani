8864789bfcad560c3bd0858fb79ecd318e831d54
Kelvin wepo <kelvin.wepo@student.moringaschool.com>
2022-06-21 03:37:42 +0300

    feat: this commit modifies admi.py

--- neighbours/admin.py
+++ neighbours/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Profile, Business, Neighborhood, Contact, Post
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Neighborhood)
+admin.site.register(Contact)
+admin.site.register(Post)
+
+
