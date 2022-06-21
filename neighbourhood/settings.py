e9fdee6e063629dfce7a0c9d2bafcfbbf99e5f72
Kelvin wepo <kelvin.wepo@student.moringaschool.com>
2022-06-21 20:14:16 +0300

    deploy

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -70,7 +70,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhitenoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'neighbourhood.urls'
